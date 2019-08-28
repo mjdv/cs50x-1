f5fac06f4fe9d87a2f19e6fb32c7f46211a9f77f
Jelle van Assema <jelle.van.assema@gmail.com>
2019-08-28 12:17:40 +0200

    Update __init__.py

--- cash/__init__.py
+++ cash/__init__.py
@@ -4,44 +4,44 @@ import check50.c
 
 @check50.check()
 def exists():
-    """cash exists"""
-    check50.exists("cash.c")
+    """greedy exists"""
+    check50.exists("greedy.c")
 
 
 @check50.check(exists)
 def compiles():
-    """cash compiles"""
-    check50.c.compile("cash.c", lcs50=True)
+    """greedy compiles"""
+    check50.c.compile("greedy.c", lcs50=True)
 
 
 @check50.check(compiles)
 def test041():
     """input of 0.41 yields output of 4"""
-    check50.run("./cash").stdin("0.41").stdout(coins(4), "4\n").exit(0)
+    check50.run("./greedy").stdin("0.41").stdout(coins(4), "4\n").exit(0)
 
 
 @check50.check(compiles)
 def test001():
     """input of 0.01 yields output of 1"""
-    check50.run("./cash").stdin("0.01").stdout(coins(1), "1\n").exit(0)
+    check50.run("./greedy").stdin("0.01").stdout(coins(1), "1\n").exit(0)
 
 
 @check50.check(compiles)
 def test015():
     """input of 0.15 yields output of 2"""
-    check50.run("./cash").stdin("0.15").stdout(coins(2), "2\n").exit(0)
+    check50.run("./greedy").stdin("0.15").stdout(coins(2), "2\n").exit(0)
 
 
 @check50.check(compiles)
 def test160():
     """input of 1.6 yields output of 7"""
-    check50.run("./cash").stdin("1.6").stdout(coins(7), "7\n").exit(0)
+    check50.run("./greedy").stdin("1.6").stdout(coins(7), "7\n").exit(0)
 
 
 @check50.check(compiles)
 def test230():
     """input of 23 yields output of 92"""
-    check50.run("./cash").stdin("23").stdout(coins(92), "92\n").exit(0)
+    check50.run("./greedy").stdin("23").stdout(coins(92), "92\n").exit(0)
 
 
 @check50.check(compiles)
@@ -49,7 +49,7 @@ def test420():
     """input of 4.2 yields output of 18"""
     from re import search
     expected = "18\n"
-    actual = check50.run("./cash").stdin("4.2").stdout()
+    actual = check50.run("./greedy").stdin("4.2").stdout()
     if not search(coins(18), actual):
         help = None
         if search(coins(22), actual):
@@ -60,19 +60,19 @@ def test420():
 @check50.check(compiles)
 def test_reject_negative():
     """rejects a negative input like -1"""
-    check50.run("./cash").stdin("-1").reject()
+    check50.run("./greedy").stdin("-1").reject()
 
 
 @check50.check(compiles)
 def test_reject_foo():
     """rejects a non-numeric input of "foo" """
-    check50.run("./cash").stdin("foo").reject()
+    check50.run("./greedy").stdin("foo").reject()
 
 
 @check50.check(compiles)
 def test_reject_empty():
     """rejects a non-numeric input of "" """
-    check50.run("./cash").stdin("").reject()
+    check50.run("./greedy").stdin("").reject()
 
 
 def coins(num):
