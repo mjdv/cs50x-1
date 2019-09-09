30c84f5f2f5ea515ffacd821f01a82308a313354
jelleas <jelle.van.assema@gmail.com>
2019-09-09 14:01:40 +0200

    initials prompt

--- initials/more/__init__.py
+++ initials/more/__init__.py
@@ -5,9 +5,9 @@ from less import *
 @check50.check(compiles)
 def space_between():
     """Outputs HJ for hailey       James"""
-    check50.run("./initials").stdin("hailey       James", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+    check50.run("./initials").stdin("hailey       James").stdout(match("HJ"), "HJ\n").exit(0)
 
 @check50.check(compiles)
 def space_before_after():
     """Outputs HJ for     hailey James    """
-    check50.run("./initials").stdin("    hailey James    ", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+    check50.run("./initials").stdin("    hailey James    ").stdout(match("HJ"), "HJ\n").exit(0)
